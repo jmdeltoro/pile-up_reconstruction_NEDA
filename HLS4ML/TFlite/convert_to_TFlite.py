62db36a5d17a5add77f169e5275224ba58b67692
Ricardo-Mart-98 <rmartinezaldea@gmail.com>
2025-07-03 18:22:53 +0200

    Creado .tflite del modelo fastrtl

--- HLS4ML/TFlite/convert_to_TFlite.py
+++ HLS4ML/TFlite/convert_to_TFlite.py
@@ -5,6 +5,8 @@
 ##################################################################################################################################################
 
 import tensorflow as tf
+from tensorflow.keras.models import load_model
+import os
 import keras.losses
 
 def customLoss(yTrue, yPred):
@@ -15,10 +17,10 @@ def customLoss(yTrue, yPred):
     return K.mean(K.square(yTrue - yPred))
 
 keras.losses.customLoss = customLoss
-keras_model = tf.keras.models.load_model(r'C:\Users\rmart\pile-up_reconstruction_NEDA\pile-up_reconstruction_NEDA\modelo_final_entrenado_autoencoder\saved_model.pb', custom_objects={'Loss':customLoss}) #path of the keras model
+keras_model = load_model(os.path.join(r"C:\Users\rmart\pile-up_reconstruction_NEDA\HLS4ML\Modelos\modelo_400_epochs_fast_rtl", 'model.h5')) #path of the keras model
 
 converter = tf.lite.TFLiteConverter.from_keras_model(keras_model)
 tflite_model = converter.convert()
 
-with open('converted_model.tflite', 'wb') as f: ##name or path where save the converted model
+with open(r'C:\Users\rmart\pile-up_reconstruction_NEDA\HLS4ML\Modelos\modelo_400_epochs_fast_rtl\converted_model_fastrtl.tflite', 'wb') as f: ##name or path where save the converted model
   f.write(tflite_model) 
